bcb95469cc73e0cc9950298920b3accdbac02944
AugustoBondanca <augustobondancaa@gmail.com>
2020-06-13 12:45:55 -0300

    sidenav com rotas criada

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  constructor() {
+        this.menu = [
+          {
+            path: 'home',
+            text: 'Home'
+          },
+          {
+            path: 'info',
+            text: 'Informações'
+          },
+          {
+            path: 'alerts',
+            text: 'Alertas'
+          },
+          {
+            path: 'message',
+            text: 'Mensagens'
+          },
+          /**{
+            path: 'initials-tasks',
+            text: 'Aprovação de Contas'
+          }**/
+        ];
+  }
+
+  menu;
+  profile: string;
+
+  isCollapsed = true;
+
+
+
+  toggle() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  collapse() {
+    this.isCollapsed = true;
+  }
+
+  ngOnInit() {
+
+  }
+}
