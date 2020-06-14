aaf8449ed73b02c24b76bf190133dd9e1e1c5373
AugustoBondanca <augustobondancaa@gmail.com>
2020-06-13 22:56:43 -0300

    correção de marcador de emergencia

--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -10,28 +10,117 @@ import * as Mapboxgl from 'mapbox-gl';
 })
 export class AlertsComponent implements OnInit {
 
-  mapa: Mapboxgl.Map;
-
   constructor() { }
 
   ngOnInit() {
-
+    var mapa: Mapboxgl.Map;
+    
     Mapboxgl.accessToken = environment.mapBoxKey;
 
-    this.mapa = new Mapboxgl.Map({
+    mapa = new Mapboxgl.Map({
       container: 'mapa-mapbox', // container id
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/streets-v9',
       center: [-46.576213, -23.4539413], // starting position
       zoom: 15.6 // starting zoom
     });
-    this.createMarker(-46.576213, -23.4539413);
-  }
 
-  createMarker(lng:number, lat: number) {
-    const marker = new Mapboxgl.Marker({
-      })
-      .setLngLat([lng, lat])
-      .addTo(this.mapa);
+      var size = 200;
+  
+      // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
+      // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
+      var pulsingDot = {
+          width: size,
+          height: size,
+          data: new Uint8Array(size * size * 4),
+  
+          // get rendering context for the map canvas when layer is added to the map
+          onAdd: function() {
+              var canvas = document.createElement('canvas');
+              canvas.width = this.width;
+              canvas.height = this.height;
+              this.context = canvas.getContext('2d');
+          },
+  
+          // called once before every frame where the icon will be used
+          render: function() {
+              var duration = 1000;
+              var t = (performance.now() % duration) / duration;
+  
+              var radius = (size / 2) * 0.3;
+              var outerRadius = (size / 2) * 0.7 * t + radius;
+              var context = this.context;
+  
+              // draw outer circle
+              context.clearRect(0, 0, this.width, this.height);
+              context.beginPath();
+              context.arc(
+                  this.width / 2,
+                  this.height / 2,
+                  outerRadius,
+                  0,
+                  Math.PI * 2
+              );
+              context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+              context.fill();
+  
+              // draw inner circle
+              context.beginPath();
+              context.arc(
+                  this.width / 2,
+                  this.height / 2,
+                  radius,
+                  0,
+                  Math.PI * 2
+              );
+              context.fillStyle = 'rgba(255, 100, 100, 1)';
+              context.strokeStyle = 'white';
+              context.lineWidth = 2 + 4 * (1 - t);
+              context.fill();
+              context.stroke();
+  
+              // update this image's data with data from the canvas
+              this.data = context.getImageData(
+                  0,
+                  0,
+                  this.width,
+                  this.height
+              ).data;
+  
+              // continuously repaint the map, resulting in the smooth animation of the dot
+              mapa.triggerRepaint();
+  
+              // return `true` to let the map know that the image was updated
+              return true;
+          }
+      };
+  
+      mapa.on('load', function() {
+        mapa.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+  
+        mapa.addSource('points', {
+              'type': 'geojson',
+              'data': {
+                  'type': 'FeatureCollection',
+                  'features': [
+                      {
+                          'type': 'Feature',
+                          'geometry': {
+                              'type': 'Point',
+                              'coordinates': [-46.576213, -23.4539413]
+                          }
+                      }
+                  ]
+              }
+          });
+          mapa.addLayer({
+              'id': 'points',
+              'type': 'symbol',
+              'source': 'points',
+              'layout': {
+                  'icon-image': 'pulsing-dot'
+              }
+          });
+      });
   }
 
 }
